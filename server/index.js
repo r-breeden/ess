a62933c1265192f07d0a2670402e565dfec4a16c
Ryan Breeden <ryan@ryanbreeden.com>
2018-02-27 15:11:58 -0800

    basic server implemented

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res, next) => {
+  res.send('greetings from serverland');
+});
+
+app.listen(PORT, function () {
+  console.log(`server online, listening on port ${PORT}.`)
+})
